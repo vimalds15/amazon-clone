e39399700de463e3f2536abeea986c9a7874329f
vimalds15 <74135002+vimalds15@users.noreply.github.com>
2021-02-20 01:26:29 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/ecommerce-ed61b/us-central1/api' //THE Api (cloud function) URL
+    baseURL: '//Enter your localhost/host' //THE Api (cloud function) URL
 });
 
 export default instance;
